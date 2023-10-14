71cc63b78a2754dbd4abc346097e9c1fd840ecf7
RayyanRahman <rayyanrahman11@gmail.com>
2023-10-14 13:10:02 +0530

    Banner Component Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Banner />
     </div>
   );
 }
