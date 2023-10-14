6df2f7b3aa97c85479139c9a9fd9d4a4445e213b
RayyanRahman <rayyanrahman11@gmail.com>
2023-10-14 13:27:20 +0530

    Search Component Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
+import SearchFilter from "./components/SearchFilter";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+
+  // Fetch data from the API
+  useEffect(() => {
+    fetch("https://api.spacexdata.com/v3/capsules")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+        setFilteredData(data);
+      });
+  }, []);
+
+  // Handle filter changes
+  const handleFilterChange = (newFilters) => {
+    const filtered = data.filter((item) => {
+      const original_launch = new Date(item.original_launch).toDateString();
+      return (
+        original_launch.includes(newFilters.original_launch.toLowerCase()) &&
+        item.status.toLowerCase().includes(newFilters.status.toLowerCase()) &&
+        item.type.toLowerCase().includes(newFilters.type.toLowerCase())
+      );
+    });
+    setFilteredData(filtered);
+  };
+
   return (
     <div>
       <NavBar />
       <Banner />
+      <SearchFilter onSearch={handleFilterChange} />
     </div>
   );
 }
